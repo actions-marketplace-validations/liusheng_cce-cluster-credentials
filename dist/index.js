b6cbbc135dbebb9fd5271e56e98013db7ae38e3b
liusheng <liusheng2048@gmail.com>
2021-03-27 08:35:50 +0000

    Regenerate release

--- dist/index.js
+++ dist/index.js
@@ -11157,13 +11157,14 @@ async function run() {
     let cluster_id = core.getInput('cluster-id', { required: true });
     let url = `https://cce.ap-southeast-3.myhuaweicloud.com/api/v3/projects/${project_id}/clusters/${cluster_id}/clustercert`;
     http_request.init_sign({AccessKey:`${ak}`, SecretKey:`${sk}`});
-    config = await http_request.http_request("GET", url, "");
+    config = await http_request.http_request("POST", url, '', {"duration": 1});
 
     const runnerTempDirectory = process.env['RUNNER_TEMP']; // Using process.env until the core libs are updated
     const kubeconfigPath = path.join(runnerTempDirectory, `kubeconfig_${Date.now()}`);
     core.debug(`Writing kubeconfig contents to ${kubeconfigPath}`);
     fs.writeFileSync(kubeconfigPath, JSON.stringify(config));
     fs.chmodSync(kubeconfigPath, '600');
+    core.setSecret(kubeconfigPath);
     core.exportVariable('KUBECONFIG', kubeconfigPath);
     console.log('KUBECONFIG environment variable is set');
 }
